e06d7cdff7ef8ecca58c6dbb3a528d8a1e1de000
FythonDev <fython.project@gmail.com>
2025-09-10 00:39:02 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+---
+
+## 📄 `setup.py`
+
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name="fython",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Persian-based programming language powered by Python",
+    author="AM1R_SHAH",
+    author_email="fython.project@gmail.com",
+    url="https://github.com/FythonDev/fython",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Natural Language :: Persian",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
