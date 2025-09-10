14072294e0492264c1e3bc866ba424387269aec7
FythonDev <fython.project@gmail.com>
2025-09-10 08:00:58 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-python
+```python
 from setuptools import setup, find_packages
 
 setup(
-    name="fython",
+    name="parspy",
     version="0.1.0",
     packages=find_packages(),
     description="Persian-based programming language powered by Python",
     author="aQa_PeTeR",
-    author_email="fython.project@gmail.com",
-    url="https://github.com/FythonDev/fython",
+    author_email="parspy.project@gmail.com",
+    url="https://github.com/FythonDev/parspy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Natural Language :: Persian",
